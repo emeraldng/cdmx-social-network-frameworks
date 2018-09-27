5a7c9b82dfcf7566cbe996fffb72005a18dcbc6c
emeraldng <ifglitterthen@gmail.com>
2018-09-27 00:26:07 -0500

    añadiendo componentes de login/register/neews-feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule
