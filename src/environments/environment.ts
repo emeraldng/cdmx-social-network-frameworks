ce2b176d99892dbe81afe20128c30d240b8d36c2
emeraldng <ifglitterthen@gmail.com>
2018-09-27 00:20:42 -0500

    Se añadió keys de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyDvmc9tLJ9_jhYm9f97Es7ectLp7ZXfuQA",
+    authDomain: "angular-meuus.firebaseapp.com",
+    databaseURL: "https://angular-meuus.firebaseio.com",
+    projectId: "angular-meuus",
+    storageBucket: "angular-meuus.appspot.com",
+    messagingSenderId: "427341719896"
+  }
 };
 
 /*
