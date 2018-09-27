0d1132e0e489b8cbec4a1efc3032c85663640a19
emeraldng <ifglitterthen@gmail.com>
2018-09-27 17:19:18 -0500

    Añadiendo las keys de FB en Environments de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // Aqui también va las keys de FB
+  firebase : {
+    apiKey: 'AIzaSyDvmc9tLJ9_jhYm9f97Es7ectLp7ZXfuQA',
+    authDomain: 'angular-meuus.firebaseapp.com',
+    databaseURL: 'https://angular-meuus.firebaseio.com',
+    projectId: 'angular-meuus',
+    storageBucket: 'angular-meuus.appspot.com',
+    messagingSenderId: '427341719896'
+  }
 };
