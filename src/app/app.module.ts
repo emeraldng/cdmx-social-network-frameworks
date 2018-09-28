5c60ce5264807f7c177284b4f1c42bb6b0e4683b
emeraldng <ifglitterthen@gmail.com>
2018-09-27 23:36:40 -0500

    Deploy 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule, NO_ERRORS_SCHEMA  } from '@angular/core';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-
-// // import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-// import { MDBBootstrapModule } from 'angular-bootstrap-md';
-// import { LoginComponent } from './login/login.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     LoginComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     MDBBootstrapModule.forRoot()
-//   ],
-//   schemas: [NO_ERRORS_SCHEMA],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,19 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-register',
-//   templateUrl: './register.component.html',
-//   styleUrls: ['./register.component.scss']
-// })
-// export class RegisterComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
-
 import { Router } from '@angular/router';
 import { AuthService } from './../shared/auth.service';
 import { Component } from '@angular/core';
